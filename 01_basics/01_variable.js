b17774d63ca59861cb2edfebfc13ea09098f3ec9
manuheckerhai <manu.singhal2005@gmail.com>
2025-03-19 15:14:25 +0000

    var and dat

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "manu@gmail.com"
+var accountPassword="12345"
+accountCity = "Delhi"
+
+// accountId =2
+accountEmail = "hc@hc.com"
+accountPassword="212121212"
+accountCity = "Banglore"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
